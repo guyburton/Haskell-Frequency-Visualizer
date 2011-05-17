6d8776c43c0782bca04c26808ff1977fb895157d
= <=>
2011-05-17 09:26:37 +0100

    Working but sketchy solution. Now using GLUT windowing library. The window doesn't redraw quite right but the output is generally correct.

--- gl.hs
+++ gl.hs
@@ -1,67 +1,51 @@
 --
 --Code based loosely on the Nehe tutorials GHC port
---
+-- 
 
 module Main where
 
--- import Graphics.UI.GLUT
-import qualified Graphics.UI.GLFW as GLFW
--- everything from here starts with gl or GL
-import Graphics.Rendering.OpenGL.Raw
+import Graphics.UI.GLUT
 import Graphics.Rendering.GLU.Raw ( gluPerspective )
+
+import Graphics.Rendering.OpenGL as GL
+import Graphics.Rendering.OpenGL.Raw
+import Data.IORef
+import Control.Monad
+
 import Data.Bits ( (.|.) )
 import System.Exit ( exitWith, ExitCode(..) )
-import Control.Monad ( forever, forM )
-import Data.IORef ( IORef, newIORef, readIORef, writeIORef )
 import Data.Colour.RGBSpace.HSV
 import Data.Colour.RGBSpace
 import Control.Concurrent.Thread
-import GuyAudio
+import System.Environment (getArgs)
+import Sound.Pulse.Simple
+import Data.WAVE
+import Numeric.FFT
 
-initGL :: IO ()
-initGL = do
-  glShadeModel gl_SMOOTH -- enables smooth color shading
-  glClearColor 0 0 0 0 -- Clear the background color to black
-  glClearDepth 1 -- enables clearing of the depth buffer
-  glEnable gl_DEPTH_TEST
-  glDepthFunc gl_LEQUAL -- type of depth test
-  glHint gl_PERSPECTIVE_CORRECTION_HINT gl_NICEST
-
-resizeScene :: GLFW.WindowSizeCallback
-resizeScene w     0      = resizeScene w 1 -- prevent divide by zero
-resizeScene width height = do
-  glViewport 0 0 (fromIntegral width) (fromIntegral height)
-  glMatrixMode gl_PROJECTION
-  glLoadIdentity
-  gluPerspective 45 (fromIntegral width/fromIntegral height) 0.1 100 
-  glMatrixMode gl_MODELVIEW
-  glLoadIdentity
-  glFlush
-
+import Data.Complex
 size :: GLfloat
 size = 0.4
 
 getBandMagnitude :: Int -> IORef [Double] -> IO(GLfloat)
 getBandMagnitude index vals = do
     listOfVals <- readIORef vals
-    return $ realToFrac $ listOfVals !! index
+    return $ (*) 0.1 $ realToFrac $ listOfVals !! index
 
-drawScene :: Int -> IORef [Double] -> IO()
-drawScene num bands = do
-  
--- clear the screen and the depth bufer
+drawScene :: IORef [Double] -> IO()
+drawScene bands = do
+  -- clear the screen and the depth bufer
   glClear $ fromIntegral  $  gl_COLOR_BUFFER_BIT
                          .|. gl_DEPTH_BUFFER_BIT
   glLoadIdentity -- reset view
   glTranslatef  (-5.0) (-4.0) (-15.0)
- 
+  b <- readIORef bands
+  let num = length b
   forM [0 .. (num-1)] $ \i -> do
     glTranslatef 1.0 0.0 0.0
     glBegin gl_QUADS
 
     -- Cycle through color spectrum for bars
-    uncurryRGB glColor3f $ hsv (fromIntegral (i * (360 div num) )) 1.0 1.0
-    
+    uncurryRGB glColor3f $ hsv (fromIntegral (i * (360 `div` num) )) 1.0 1.0
     -- Get magnitude for the current bar
     mag <- getBandMagnitude i bands
 
@@ -99,64 +83,83 @@ drawScene num bands = do
     glEnd
   
   glFlush
+  swapBuffers
 
-shutdown :: GLFW.WindowCloseCallback
-shutdown = do
-  GLFW.closeWindow
-  GLFW.terminate
-  _ <- exitWith ExitSuccess
-  return True
+resizeCallback size@(Size width height) = do
+        glViewport 0 0 (fromIntegral width) (fromIntegral height)
+        glMatrixMode gl_PROJECTION
+        glLoadIdentity
+        gluPerspective 45 (fromIntegral width/fromIntegral height) 0.1 100 
+        glMatrixMode gl_MODELVIEW
+        glLoadIdentity
+        glFlush
 
-keyPressed :: GLFW.KeyCallback 
-keyPressed GLFW.KeyEsc True = shutdown >> return ()
-keyPressed _           _    = return ()
+playChunk s window samplesRef bands = do
+	samples <- readIORef samplesRef
+	samplesPlayed <- playAudioChunk s bands $ samples
+	writeIORef samplesRef $ drop samplesPlayed samples
+	postRedisplay $ Just window
+
+start :: String -> IO()
+start input = do
+     let progName = "Guys FreqAnalyzer"
+     initialize progName []
+     window <- createWindow progName
+     windowSize $= (Size 640 480)
+     actionOnWindowClose $= Exit
+
+     bands <- newIORef [0.0]
+     writeIORef bands [0..7]
+     
+     -- register the function to do all our OpenGL drawing
+     displayCallback $= (drawScene bands)
+     -- register the function called when our window is resized
+     reshapeCallback $= Just resizeCallback
+
+     glShadeModel gl_SMOOTH -- enables smooth color shading
+     glHint gl_PERSPECTIVE_CORRECTION_HINT gl_NICEST
+     glClearColor 0 0 0 0 -- Clear the background color to black$
+     glClearDepth 1 -- enables clearing of the depth buffer$
+     glEnable gl_DEPTH_TEST
+     glDepthFunc gl_LEQUAL -- type of depth test$
+     
+     s <- simpleNew Nothing "example" Play Nothing "FreqAnalyzer" (SampleSpec (F32 LittleEndian) 44100 1) Nothing Nothing
+     wave <- getWAVEFile input
+     samplesRef <- newIORef $ getChannel 1 wave     
+     idleCallback    $= Just (playChunk s window samplesRef bands)
+
+     mainLoop
+
+
+-- Gets a list of samples for the specified channel from the wave file
+getChannel :: Int -> WAVE -> [Float]
+getChannel channel wave = map (\x -> realToFrac . sampleToDouble $ x !! channel) $ waveSamples wave
+
+doFFT :: [Float] -> [Double]
+doFFT samples = map magnitude . fft $ map (\x -> realToFrac x :+ 0) $ samples
+
+foldFFT :: Int -> [Double] -> [Double]
+foldFFT 0 _ = []
+foldFFT bins binMags = 
+    [sum $ take num binMags] ++ foldFFT (num-1) (drop num binMags)
+    where 
+        num = length binMags `div` bins
+
+-- Recursively play complete track in sample chunks
+-- Work out the FFT of the first section and write to IO ref
+playAudioChunk :: Simple -> IORef [Double] -> [Float] -> IO(Int)
+playAudioChunk s bands samples = do
+    let buffer = take 8192 samples
+    simpleWrite s buffer
+    writeIORef bands $ foldFFT 8 $ doFFT $ take 256 samples
+    return 8192    
 
 main :: IO ()
 main = do
     args <- getArgs
     case args of
         [input] -> do
-            bands <- newIORef [0.0]
-            writeIORef bands [0..7]
-            updateThread <- forkIO $ playAudio input bands
-            initGL bands
+            start input
         _ -> putStrLn "Must specify filename"
-    
-initGL :: IORef [Double] -> IO()
-initGL bands = do
-     True <- GLFW.initialize 
-     -- get a 640 x 480 window
-     let dspOpts = GLFW.defaultDisplayOptions
-                     { GLFW.displayOptions_width  = 640
-                     , GLFW.displayOptions_height = 480
-                     -- Set depth buffering and RGBA colors
-                     , GLFW.displayOptions_numRedBits   = 8
-                     , GLFW.displayOptions_numGreenBits = 8
-                     , GLFW.displayOptions_numBlueBits  = 8
-                     , GLFW.displayOptions_numAlphaBits = 8
-                     , GLFW.displayOptions_numDepthBits = 1
-                     -- , GLFW.displayOptions_displayMode  = GLFW.Fullscreen
-                     } 
-     
-     -- initialize our window.
-     True <- GLFW.openWindow dspOpts
-     -- window starts at upper left corner of the screen
-     GLFW.setWindowPosition 0 0
-     -- open a window
-     GLFW.setWindowTitle "Guys FreqAnalyzer"
-     -- register the function to do all our OpenGL drawing
-     GLFW.setWindowRefreshCallback (drawScene 8 bands)
-     -- register the funciton called when our window is resized
-     GLFW.setWindowSizeCallback resizeScene
-     -- register the function called when the keyboard is pressed.
-     GLFW.setKeyCallback keyPressed
-     -- register window close handler
-     
-     GLFW.setWindowCloseCallback shutdown
-     initGL
 
-     -- start event processing engine
-     forever $ do
-       drawScene 8 bands
-       GLFW.swapBuffers
 
